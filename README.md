114d96559451a2ad2a6fc412c5c9bfd48db15a74
Adeola David Adelakun <aadelakun28@gmail.com>
2023-05-28 06:35:33 +0100

    Chat Interface added

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # chatbot-by-adesdesk
-### chatbot-by-adesdesk is a frontend chat application created with React.js that sends an API call to OpenAI's ChatGPT API endpoint allowing users to chat and get real time AI feedback on prompts made.
+### chatbot-by-adesdesk is a frontend chat application created as a Vitte + Vue project that sends an API call to OpenAI's ChatGPT API endpoint allowing users to chat and get real time AI feedback on prompts made.
---
+++ src/components/Chatbot.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="chatbot">
+    <div class="chat-window">
+      <div v-for="(message, index) in messages" :key="index" :class="`message ${message.type}`">
+        {{ message.message }}
+      </div>
+    </div>
+    <p>Hello</p>
+    <form @submit="handleSubmit">
+      <input type="text" v-model="userInput" placeholder="Type your message here" />
+      <button type="submit">Send</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  name: 'Chatbot',
+  setup() {
+    const messages = ref([]);
+    const userInput = ref('');
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      const userMessage = userInput.value;
+      messages.value.push({ type: 'user', message: userMessage });
+      userInput.value = '';
+    };
+
+    return {
+      messages,
+      userInput,
+      handleSubmit,
+    };
+  },
+};
+</script>
+
+<style>
+.chatbot {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.chat-window {
+  width: 400px;
+  height: 500px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  overflow-y: scroll;
+}
+
+.message {
+margin-bottom: 10px;
+  padding: 5px;
+}
+
+.user {
+  text-align: right;
+  background-color: #0095ff;
+  color: #fff;
+}
+
+.chatbot {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.chat-window {
+  width: 400px;
+  height: 500px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  overflow-y: scroll;
+}
+
+.message {
+  margin-bottom: 10px;
+  padding: 5px;
+}
+
+.user {
+  text-align: right;
+  background-color: #0095ff;
+  color: #fff;
+}
+
+.bot {
+  text-align: left;
+  background-color: #f5f5f5;
+  color: #333;
+}
+
+/* Add your styles here */
+</style>
